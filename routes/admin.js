e4d106f98d9d3f63910020d326ed1a828a7a156d
xtrg1202 <thatkhongthetinduoc12@gmail.com>
2024-04-29 17:46:28 +0700

    design: admin layout

--- routes/admin.js
+++ routes/admin.js
@@ -8,14 +8,23 @@ const CustomerModel = require('../models/customer');
 // const OrderStatus = require('../constants/order-status');
 
 router.get('/', async (req, res) => {
+    // Dashboard.... Chinh sau
     const order = await OrderModel.find().count();
     const product = await ProductModel.find().count();
 
     
     res.render("admin/index", {
+    layout: "admin/layout/layout",
     order: order,
     product: product
     });
 });
+
+router.get('/category/list', async (req, res) => {
+
+    res.render('category/list' , {
+        
+    })
+})
 
 module.exports = router;
