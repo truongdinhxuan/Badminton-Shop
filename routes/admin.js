7afdad8f32a95f199be05fe39e6480c0464372f7
truongdinhxuan <thatkhongthetinduoc12@gmail.com>
2024-12-12 16:26:48 +0700

    feat: admin product validate

--- routes/admin.js
+++ routes/admin.js
@@ -383,163 +383,163 @@ router.get('/delete-all-brand', async (req, res) => {
 })
 
 // ORDER
-router.get('/order/isdeleted', async (req,res) => {
-  const deleteOrder = await OrderModel.find({statusId: 11}).lean()
+// router.get('/order/isdeleted', async (req,res) => {
+//   const deleteOrder = await OrderModel.find({statusId: 11}).lean()
 
-  res.render('admin/order/isdeleted',{
-    layout: 'admin/layout/layout',
-    deleteOrder: deleteOrder
-  })
-})
-router.get('/order', async (req ,res) => {
-  const orders = await OrderModel.find({isDelete: false}).lean();
+//   res.render('admin/order/isdeleted',{
+//     layout: 'admin/layout/layout',
+//     deleteOrder: deleteOrder
+//   })
+// })
+// router.get('/order', async (req ,res) => {
+//   const orders = await OrderModel.find({isDelete: false}).lean();
   
-  const orderData = await Promise.all(
-    orders.map(async (order) => {
-        const status = await StatusModel.findOne({id: order.statusId}).lean();
-        const report = await ReportModel.findOne({ orderId: order.orderCode, isAccepted: false}).lean();
-        return {
-            ...order,
-            iscancelled: status && status.name === 'Canceled' && order.paymentMethod === 'bank',
-            statusName: status ? status.name : 'Unknow',
-            reportTitle: report ? report.title : null,
-            reportMessage: report ? report.message : null,
-        };
-    })
-  );
+//   const orderData = await Promise.all(
+//     orders.map(async (order) => {
+//         const status = await StatusModel.findOne({id: order.statusId}).lean();
+//         const report = await ReportModel.findOne({ orderId: order.orderCode, isAccepted: false}).lean();
+//         return {
+//             ...order,
+//             iscancelled: status && status.name === 'Canceled' && order.paymentMethod === 'bank',
+//             statusName: status ? status.name : 'Unknow',
+//             reportTitle: report ? report.title : null,
+//             reportMessage: report ? report.message : null,
+//         };
+//     })
+//   );
 
-  // console.log(orderData)
+//   // console.log(orderData)
 
-  const countReport = await ReportModel.countDocuments({isAccepted: false})
-  const countCanceledOrder = await OrderModel.findOne({statusId: 11, paymentMethod: 'bank'}).countDocuments()
-  // const bigData = await Promise.all(orders.map(async (order) => {
-  //   const status = await StatusModel.findOne({id: order.statusId}).lean();
-  //   return {
-  //     ...order,
-  //     statusName: status ? status.name : 'Unknow'
-  //   };
-  // }))   
-  res.render('admin/order' , {
-    layout: 'admin/layout/layout',
-    // data: bigData,
-    order: orderData,
-    countReport: countReport,
-    countCanceledOrder: countCanceledOrder
-  })
-})
+//   const countReport = await ReportModel.countDocuments({isAccepted: false})
+//   const countCanceledOrder = await OrderModel.findOne({statusId: 11, paymentMethod: 'bank'}).countDocuments()
+//   // const bigData = await Promise.all(orders.map(async (order) => {
+//   //   const status = await StatusModel.findOne({id: order.statusId}).lean();
+//   //   return {
+//   //     ...order,
+//   //     statusName: status ? status.name : 'Unknow'
+//   //   };
+//   // }))   
+//   res.render('admin/order' , {
+//     layout: 'admin/layout/layout',
+//     // data: bigData,
+//     order: orderData,
+//     countReport: countReport,
+//     countCanceledOrder: countCanceledOrder
+//   })
+// })
 
-router.get('/update-order/:id', async (req,res) => {
-  const orderId = req.params.id
-  const updateOrder = await OrderModel.findById(orderId).lean()
-  let orderCode = updateOrder.orderCode
-  const status = await StatusModel.find({}).lean()
-  const report = await ReportModel.findOne({ orderId: orderCode }).lean();
+// router.get('/update-order/:id', async (req,res) => {
+//   const orderId = req.params.id
+//   const updateOrder = await OrderModel.findById(orderId).lean()
+//   let orderCode = updateOrder.orderCode
+//   const status = await StatusModel.find({}).lean()
+//   const report = await ReportModel.findOne({ orderId: orderCode }).lean();
 
-  const selectedStatus = status.find(status => status.id === updateOrder.statusId)
+//   const selectedStatus = status.find(status => status.id === updateOrder.statusId)
 
-  let activeSteps = [];
-  let showreturnsteps = false;
+//   let activeSteps = [];
+//   let showreturnsteps = false;
 
-  if (updateOrder.statusId >= 6 && updateOrder.statusId <= 10) {
-    showreturnsteps = true; // Hiển thị bước hoàn trả hàng nếu status là 6, 7, hoặc 8
-  }
-  // hoàn trả
-  if (updateOrder.statusId === 6) {
-    activeSteps = [6]
-  } else if (updateOrder.statusId === 7) {
-    activeSteps = [6,7]
-  } else if (updateOrder.statusId === 8) {
-    activeSteps = [6,7.8]
-  // giao hàng
-  } else if (updateOrder.statusId === 1) {
-    activeSteps = [1]; // chỉ step 1 active
-  } else if (updateOrder.statusId === 2) {
-    activeSteps = [1, 2]; // step 1 và 2 active
-  } else if (updateOrder.statusId === 3) {
-    activeSteps = [1, 2, 3]; // step 1, 2 và 3 active
-  } else if (updateOrder.statusId == 4) {
-    activeSteps = [1,2,3,4]
-  } else if (updateOrder.statusId == 5) {
-    activeSteps = [1,2,3,4,5]
-  }
+//   if (updateOrder.statusId >= 6 && updateOrder.statusId <= 10) {
+//     showreturnsteps = true; // Hiển thị bước hoàn trả hàng nếu status là 6, 7, hoặc 8
+//   }
+//   // hoàn trả
+//   if (updateOrder.statusId === 6) {
+//     activeSteps = [6]
+//   } else if (updateOrder.statusId === 7) {
+//     activeSteps = [6,7]
+//   } else if (updateOrder.statusId === 8) {
+//     activeSteps = [6,7.8]
+//   // giao hàng
+//   } else if (updateOrder.statusId === 1) {
+//     activeSteps = [1]; // chỉ step 1 active
+//   } else if (updateOrder.statusId === 2) {
+//     activeSteps = [1, 2]; // step 1 và 2 active
+//   } else if (updateOrder.statusId === 3) {
+//     activeSteps = [1, 2, 3]; // step 1, 2 và 3 active
+//   } else if (updateOrder.statusId == 4) {
+//     activeSteps = [1,2,3,4]
+//   } else if (updateOrder.statusId == 5) {
+//     activeSteps = [1,2,3,4,5]
+//   }
 
-  // const formatMoney = new Intl.NumberFormat('vi-VI', {
-  //   style: 'currency',
-  //   currency: 'VND'
-  // }).format(updateOrder.amount)
+//   // const formatMoney = new Intl.NumberFormat('vi-VI', {
+//   //   style: 'currency',
+//   //   currency: 'VND'
+//   // }).format(updateOrder.amount)
   
-  res.render('admin/order/update-order', {
-    layout: 'admin/layout/layout',
-    updateOrder: updateOrder,
-    status: status,
-    selectedStatus: selectedStatus,
-    report: report,
-    activeSteps: activeSteps,
-    showreturnsteps: showreturnsteps,
+//   res.render('admin/order/update-order', {
+//     layout: 'admin/layout/layout',
+//     updateOrder: updateOrder,
+//     status: status,
+//     selectedStatus: selectedStatus,
+//     report: report,
+//     activeSteps: activeSteps,
+//     showreturnsteps: showreturnsteps,
    
-  })
-})
-router.post('/update-order/:id', async (req, res) => {
-  try { 
-    const orderId = req.params.id;
-    const data = req.body;
-    const updatedOrder = await OrderModel.findByIdAndUpdate(orderId, data, { new: true }).lean();
-    if (!updatedOrder) {
-      console.error(`Order with ID ${orderId} not found`);
-      return res.status(404).send("Order not found");
-    }
-    const orderCode = updatedOrder.orderCode;
-    if (!orderCode) {
-      console.error("Order code is missing");
-      return res.status(400).send("Order code is missing");
-    }
-    await ReportModel.findOneAndDelete({ orderId: orderCode }).lean();
+//   })
+// })
+// router.post('/update-order/:id', async (req, res) => {
+//   try { 
+//     const orderId = req.params.id;
+//     const data = req.body;
+//     const updatedOrder = await OrderModel.findByIdAndUpdate(orderId, data, { new: true }).lean();
+//     if (!updatedOrder) {
+//       console.error(`Order with ID ${orderId} not found`);
+//       return res.status(404).send("Order not found");
+//     }
+//     const orderCode = updatedOrder.orderCode;
+//     if (!orderCode) {
+//       console.error("Order code is missing");
+//       return res.status(400).send("Order code is missing");
+//     }
+//     await ReportModel.findOneAndDelete({ orderId: orderCode }).lean();
 
-    res.redirect('/admin/order');
-  } catch (errors) {
-    console.error("Error updating order:", errors);
-    res.redirect('/admin');
-  }
-});
-router.post('/order/update-status-order/:id', async (req, res) => {
-  try {
-      // Get the order ID from the URL
-      const id = req.params.id;
-      const newStatusId = req.body.action === 'accept' ? 7 : 8; // Example: 7 for "Accepted", 8 for "Refused"
+//     res.redirect('/admin/order');
+//   } catch (errors) {
+//     console.error("Error updating order:", errors);
+//     res.redirect('/admin');
+//   }
+// });
+// router.post('/order/update-status-order/:id', async (req, res) => {
+//   try {
+//       // Get the order ID from the URL
+//       const id = req.params.id;
+//       const newStatusId = req.body.action === 'accept' ? 7 : 8; // Example: 7 for "Accepted", 8 for "Refused"
       
-      // Update the order's status
-      const updatedOrder = await OrderModel.findOneAndUpdate(
-          { _id: id },
-          { statusId: newStatusId },
-          { new: true }
-      );
+//       // Update the order's status
+//       const updatedOrder = await OrderModel.findOneAndUpdate(
+//           { _id: id },
+//           { statusId: newStatusId },
+//           { new: true }
+//       );
 
-      if (updatedOrder) {
-          // If the order is updated, proceed to update the report
-          const report = await ReportModel.findOne({ orderId: updatedOrder.orderCode }).lean(); // Use orderCode from OrderModel to find the report
-          if (report) {
-              // Update isAccepted status in ReportModel
-              await ReportModel.findOneAndUpdate(
-                  { orderId: updatedOrder.orderCode }, // Ensure the report is matched using orderCode
-                  { isAccepted: req.body.action === 'accept' }, // Set isAccepted based on the action (accept or not)
-                  { new: true }
-              );
-              req.flash('success', `Order status updated to ${newStatusId === 7 ? 'Accepted' : 'Refused'}`);
-          } else {
-              req.flash('error', 'Report not found for the order.');
-          }
-      } else {
-          req.flash('error', 'Order not found or could not be updated.');
-      }
+//       if (updatedOrder) {
+//           // If the order is updated, proceed to update the report
+//           const report = await ReportModel.findOne({ orderId: updatedOrder.orderCode }).lean(); // Use orderCode from OrderModel to find the report
+//           if (report) {
+//               // Update isAccepted status in ReportModel
+//               await ReportModel.findOneAndUpdate(
+//                   { orderId: updatedOrder.orderCode }, // Ensure the report is matched using orderCode
+//                   { isAccepted: req.body.action === 'accept' }, // Set isAccepted based on the action (accept or not)
+//                   { new: true }
+//               );
+//               req.flash('success', `Order status updated to ${newStatusId === 7 ? 'Accepted' : 'Refused'}`);
+//           } else {
+//               req.flash('error', 'Report not found for the order.');
+//           }
+//       } else {
+//           req.flash('error', 'Order not found or could not be updated.');
+//       }
 
-      // Redirect back to the orders page
-      res.redirect(`/admin/update-order/${id}`);
-  } catch (error) {
-      console.error('Error updating order status:', error);
-      req.flash('error', 'An error occurred while updating the order status.');
-      res.redirect('/admin/order');
-  }
-});
+//       // Redirect back to the orders page
+//       res.redirect(`/admin/update-order/${id}`);
+//   } catch (error) {
+//       console.error('Error updating order status:', error);
+//       req.flash('error', 'An error occurred while updating the order status.');
+//       res.redirect('/admin/order');
+//   }
+// });
 // customer
 router.get('/account/customer', async (req,res) =>{
   const customer = await CustomerModel.find().lean()
